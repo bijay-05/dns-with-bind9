8a2acad094886d1c889ab23b06aa45d4436e2db0
bijay-05 <hawkeyesniper69@gmail.com>
2025-02-16 19:19:58 +0545

    initialize infra with terraform

---
+++ infra/main.tf
@@ -0,0 +1,71 @@
+resource "azurerm_resource_group" "testrg" {
+    name = "devtestrg"
+    location = "centralindia"
+    tags = {
+        environment = "dev"
+    }
+}
+
+resource "azurerm_virtual_network" "testvnet" {
+    name = "devtestvnet"
+    resource_group_name = azurerm_resource_group.testrg.name
+    location = azurerm_resource_group.testrg.location
+    address_space = ["10.123.0.0/16"]
+
+    tags = {
+        environment = "dev"
+    }
+}
+
+resource "azurerm_subnet" "testsubnet" {
+    name = "devtestsubnet"
+    resource_group_name = azurerm_resource_group.testrg.name
+    virtual_network_name = azurerm_virtual_network.testvnet.name
+    address_prefixes = ["10.123.1.0/24"]
+}
+
+module "network_security" {
+    source = "./modules/vm_network_security"
+    rg_name = azurerm_resource_group.testrg.name
+    rg_location = azurerm_resource_group.testrg.location
+    subnet_id = azurerm_subnet.testsubnet.id
+    nsg_name = "devtestnsg"
+    nsr_name = "devtestnsr"
+}
+
+module "network_interface_a" {
+    source = "./modules/vm_network_interface"
+    rg_name = azurerm_resource_group.testrg.name
+    rg_location = azurerm_resource_group.testrg.location
+    ip_name = "devtestipa"
+    nic_name = "devtestnica"
+    subnet_id = azurerm_subnet.testsubnet.id
+}
+
+module "network_interface_b" {
+    source = "./modules/plain_vm_network_interface"
+    rg_name = azurerm_resource_group.testrg.name
+    rg_location = azurerm_resource_group.testrg.location
+    nic_name = "devtestnicb"
+    subnet_id = azurerm_subnet.testsubnet.id
+}
+
+module "linux_vm_a" {
+    source = "./modules/linuxvm"
+    rg_name = azurerm_resource_group.testrg.name
+    rg_location = azurerm_resource_group.testrg.location
+    vm_name = "devtestvma"
+    vm_size = "Standard_B2s"
+    admin_username = "devusera"
+    nic_id = module.network_interface_a.nic_id
+}
+
+module "linux_vm_b" {
+    source = "./modules/linuxvm"
+    rg_name = azurerm_resource_group.testrg.name
+    rg_location = azurerm_resource_group.testrg.location
+    vm_name = "devtestvmb"
+    vm_size = "Standard_B1s"
+    admin_username = "devuserb"
+    nic_id = module.network_interface_b.nic_id
+}
---
+++ infra/modules/vm_network_security/variables.tf
@@ -0,0 +1,25 @@
+variable rg_name {
+    type = string
+    default = "testrg"
+}
+
+variable rg_location {
+    type = string
+    default = "us-east-1"
+}
+
+variable subnet_id {
+    type = string
+    default = "fdsfds"
+}
+
+variable nsg_name {
+    type = string
+    default = "testnsg"
+}
+
+variable nsr_name {
+    type = string
+    default = "testnsr"
+}
+
